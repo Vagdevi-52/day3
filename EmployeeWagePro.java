edf43a65b0fe2479268df6433fb6b2af75ad45d6
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-06-21 09:53:21 -0700

    Added:emp daily wage

--- EmployeeWagePro.java
+++ EmployeeWagePro.java
@@ -5,13 +5,21 @@ import java.util.Random;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+                int WAGE_PER_HR=20;
+                int FULLDAY_HRS=8;
+                int daily_wage;
 		Random rand = new Random();
 		int random = rand.nextInt(2);
-		if (random == 1)
-			System.out.println("Employee-Absent");
-		else
-			System.out.println("Employee-Present");
-	}
-    }
+		if (random == 1){
+                   
+	         	System.out.println("Employee-present");
+                        daily_wage=FULLDAY_HRS*WAGE_PER_HR;
+		        System.out.println("Employee wage is " +daily_wage);                
+		}
+                else
+                System.out.println("Employee is absent");
+           }
+        }
+
 
 
