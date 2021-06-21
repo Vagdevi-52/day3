5b53d8d7c2b8f64f4ba0364f424f4b04156b6db6
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-06-21 11:31:35 -0700

    monthlywage

--- EmployeeWagePro.java
+++ EmployeeWagePro.java
@@ -9,24 +9,43 @@ import java.util.Random;
                 int FULLDAY_HRS=8;
                 int daily_wage;
                 int PART_TIME=4;
-		Random rand = new Random();
-		int random = rand.nextInt(2);
-		if (random == 1){
-
-	         	System.out.println("Employee-present");
-                        daily_wage=FULLDAY_HRS*WAGE_PER_HR;
-		        System.out.println("Employee wage is " +daily_wage);                
-		}
-                  else if(random ==2 )
+                int num_of_workingdays=20;
+                for(int day=0;day<num_of_workingdays; day++)
                 {
-                System.out.println("Employee is part time");
-                daily_wage=PART_TIME*WAGE_PER_HR;
-                System.out.println("Employee wage is " +daily_wage);
-              }
-                else
-                System.out.println("employee is absent");
-            }
-         }
+		Random rand = new Random();
+		int random = rand.nextInt(3);
+		//if (random == 1)
+	         	//System.out.println("Employee-present");
+                        //daily_wage=FULLDAY_HRS*WAGE_PER_HR;
+		        //System.out.println("Employee wage is " +daily_wage);                
+		//}
+                  //else if(random ==2 )
+                //{
+                //System.out.println("Employee is part time");
+                //daily_wage=PART_TIME*WAGE_PER_HR;
+                //System.out.println("Employee wage is " +daily_wage);
+              //}
+                //else
+              //  System.out.println("employee is absent");
+            //}
+         //}
 
-
+switch(random)
+{
+case 1:
+daily_wage=FULLDAY_HRS*WAGE_PER_HR;
+System.out.println("Employee is Present ");
+System.out.println("Employee wage is " +daily_wage);
+break;
+case 2:
+daily_wage=PART_TIME*WAGE_PER_HR;
+System.out.println("Employee is Part time ");
+System.out.println("Employee wage is " +daily_wage);
+break;
+default:
+System.out.println("Employee is Absent");
+}
+}
+}
+}
 
