97f5fe014e0c1d3e05436906ff742852420540af
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-06-21 10:12:10 -0700

    Added:empparttime

--- EmployeeWagePro.java
+++ EmployeeWagePro.java
@@ -8,18 +8,25 @@ import java.util.Random;
                 int WAGE_PER_HR=20;
                 int FULLDAY_HRS=8;
                 int daily_wage;
+                int PART_TIME=4;
 		Random rand = new Random();
 		int random = rand.nextInt(2);
 		if (random == 1){
-                   
+
 	         	System.out.println("Employee-present");
                         daily_wage=FULLDAY_HRS*WAGE_PER_HR;
 		        System.out.println("Employee wage is " +daily_wage);                
 		}
+                  else if(random ==2 )
+                {
+                System.out.println("Employee is part time");
+                daily_wage=PART_TIME*WAGE_PER_HR;
+                System.out.println("Employee wage is " +daily_wage);
+              }
                 else
-                System.out.println("Employee is absent");
-           }
-        }
+                System.out.println("employee is absent");
+            }
+         }
 
 
 
